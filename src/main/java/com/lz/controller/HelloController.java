a96a21a07d83d2ada5a4795c8a19317fdd21ffea
liuze <1501349281@qq.com>
2023-03-21 11:11:02 +0800

    first commit

---
+++ src/main/java/com/lz/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.lz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("")
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Spring Boot !";
+    }
+
+
+
+}
